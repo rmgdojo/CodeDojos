bcdce9b6dc2d17af63f1e97a305404b1b213f026
Neil Hewitt <neil.hewitt@gmail.com>
2024-02-26 11:09:18 +0000

    CHIP-8 Challenge skeleton commit (for real this time)

---
+++ Chip-8 Challenge/src/CHIP-8 Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CHIP_8_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Chip-8 Challenge/src/CHIP-8 Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace CHIP_8_Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            // just checking NUnit is working
+            Assert.Pass();
+        }
+    }
+}
