79584e947dda687cbe071b855b32d92846f43539
Ricardo Ferreira <ricardo.b-ferreira@royalmail.com>
2024-03-11 13:48:58 +0000

    3rd week changes: optimised instructions and twelve bit construction

--- Chip-8 Challenge/src/CHIP-8 Virtual Machine/TwelveBit.cs
+++ Chip-8 Challenge/src/CHIP-8 Virtual Machine/TwelveBit.cs
@@ -1,23 +1,21 @@
-﻿namespace CHIP_8_Virtual_Machine
+﻿namespace CHIP_8_Virtual_Machine;
+
+// 12-bit address type
+public struct TwelveBit
 {
-    // 12-bit address type
-    public struct TwelveBit
+    public const int MAX_ADDRESS = 4095;
+
+    private ushort _value;
+
+    public TwelveBit(Nybble nybble1, Nybble nybble2, Nybble nybble3)
     {
-        public const int MAX_ADDRESS = 4095;
-    
-        private ushort _value;
-    
-        public TwelveBit(ushort value)
-                {
-                if (value > MAX_ADDRESS)
-                        {
-                        throw new ArgumentOutOfRangeException($"Address is greater than {MAX_ADDRESS}");
-                    }
+        var value = (ushort)(nybble1 << 8 | nybble2 << 4 | nybble3);
         
-                _value = value;
-            }
-    
-        public static implicit operator ushort(TwelveBit address) => address._value;
-        public static implicit operator TwelveBit(ushort value) => new TwelveBit(value);
+        if (value > MAX_ADDRESS)
+        {
+            throw new ArgumentOutOfRangeException($"Address is greater than {MAX_ADDRESS}");
+        }
+        
+        _value = value;
     }
 }
