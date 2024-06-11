53938e66b8b5000adf52f9c740d20832f04cf110
Neil Hewitt <neil.hewitt@gmail.com>
2024-06-11 14:36:07 +0100

    Refactor keypad class. Remove temporary test.

--- Chip-8 Challenge/src/CHIP-8 Console/Program.cs
+++ Chip-8 Challenge/src/CHIP-8 Console/Program.cs
@@ -6,30 +6,6 @@ namespace CHIP_8_Console
     {
         static void Main(string[] args)
         {
-            Keypad keypad = new();
-            while (true)
-            {
-                try
-                {
-                    var key = Console.ReadKey(false);
-
-                    keypad.KeyDown(key.KeyChar.ToString());
-                    if (keypad.IsDown(0x01))
-                    {
-                        Console.WriteLine("Key is down!");
-                    }
-
-                    keypad.KeyUp(key.KeyChar.ToString());
-                    if (!keypad.IsDown(0x01))
-                    {
-                        Console.WriteLine("Key is up!");
-                    }
-                }
-                catch (KeyNotFoundException)
-                {
-                    Console.WriteLine("Key is not mapped.");
-                }
-            }
         }
     }
 }
