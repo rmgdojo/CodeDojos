3d13d90a952d2f0334f5e61cb1ce2cf847719ee0
Mark Lowe <mark.a.lowe@royalmail.com>
2024-10-02 15:04:40 +0100

    Add mock IKeypadMap when instantiating a VM for testing in InstructionsTestBase

--- Chip-8 Challenge/src/CHIP-8 Tests/Instructions/InstructionTestsBase.cs
+++ Chip-8 Challenge/src/CHIP-8 Tests/Instructions/InstructionTestsBase.cs
@@ -1,14 +1,36 @@
-﻿using NUnit.Framework;
+﻿using Castle.Components.DictionaryAdapter;
+using NUnit.Framework;
 using CHIP_8_Virtual_Machine;
 using CHIP_8_Virtual_Machine.InstructionBases;
+using NSubstitute;
 
 namespace CHIP_8_Tests.Instructions
 {
     [TestFixture]
     public abstract class InstructionTestsBase
     {
-        public string WINKEY_1 = "D1";
+        public string WINKEY_1 = "1";
         public Nibble KEYPAD_0 = 0;
+
+        private IKeypadMap _keyboardMap;
+        private readonly IDictionary<string, Nibble> _mockKeyboardMapValues = new Dictionary<string, Nibble> {
+            { "1", 0x0 },
+            { "2", 0x1 },
+            { "3", 0x2 },
+            { "4", 0x3 },
+            { "Q", 0x4 },
+            { "W", 0x5 },
+            { "E", 0x6 },
+            { "R", 0x7 },
+            { "A", 0x8 },
+            { "S", 0x9 },
+            { "D", 0xA },
+            { "F", 0xB },
+            { "Z", 0xC },
+            { "X", 0xD },
+            { "C", 0xE },
+            { "V", 0xF }
+        };
 
         public VM VM { get; set; }
 
@@ -27,7 +49,12 @@ namespace CHIP_8_Tests.Instructions
         [SetUp]
         public void Setup()
         {
-            VM = new VM(new WindowsKeypadMap());
+            _keyboardMap = Substitute.For<IKeypadMap>();
+            _keyboardMap.Map.Returns(_mockKeyboardMapValues);
+
+            VM = new VM(_keyboardMap);
         }
+
+
     }
 }
