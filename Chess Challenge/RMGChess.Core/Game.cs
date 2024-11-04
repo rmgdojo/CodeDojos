a41c1ddd0ede4d838f56106925409231ca7bfa44
Neil Hewitt <neil.hewitt@gmail.com>
2024-11-04 13:07:04 +0000

    Week 1 initial commit.

---
+++ Chess Challenge/RMGChess.Core/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace RMGChess.Core
+{
+    public class Game
+    {
+        public Game()
+        {
+            Board board = new Board();
+        }
+    }
+
+    public class Board
+    {
+        private Square[,] _squares;
+
+        public Square this[int x, int y] => _squares[x, y];
+
+        public Board()
+        {
+            _squares = new Square[8, 8];
+        }
+    }
+
+    public class Square
+    {
+    }
+}
