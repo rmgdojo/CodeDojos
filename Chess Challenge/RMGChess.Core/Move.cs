df8ee303384cc999c4a1b8b329f10a63cdebc64e
Neil Hewitt <neil.hewitt@gmail.com>
2025-03-03 13:42:19 +0000

    Just some tiny refactoring to make it good 'n stuff

--- Chess Challenge/RMGChess.Core/Move.cs
+++ Chess Challenge/RMGChess.Core/Move.cs
@@ -10,17 +10,29 @@ namespace RMGChess.Core
     {
         public static Move DecodeAlgebra(string moveAsAlgebra, Board board, Colour whoIsMoving)
         {
-            if (String.IsNullOrWhiteSpace(moveAsAlgebra))
+            // check if empty or too short to be valid
+            if (String.IsNullOrWhiteSpace(moveAsAlgebra) || moveAsAlgebra.Length < 2)
             {
                 throw new ArgumentException("Algebra cannot be empty.");
             }
 
+            // is this a castling move?
             (bool castling, CastlingType castlingType) = moveAsAlgebra switch
             {
                 "O-O" => (true, CastlingType.Kingside),
                 "O-O-O" => (true, CastlingType.Queenside),
                 _ => (false, CastlingType.None)
             };
+
+            // validate that the algebra ends with a valid position (e4 etc)
+            try
+            {
+                Position to = moveAsAlgebra[^2..];
+            }
+            catch
+            {
+                throw new ArgumentException("Algebra must end with a valid position.");
+            }
 
             bool takesPiece = moveAsAlgebra.Contains("x");
             Move move = null;
@@ -41,54 +53,68 @@ namespace RMGChess.Core
 
             if (!castling)
             {
+                // we need to work out which piece is moving via induction (unless it's stated)
+
                 Piece piece = null;
                 var validMoves = board.GetValidMovesForAllPieces();
+                moveAsAlgebra = moveAsAlgebra.TrimEnd('#', '+'); // remove warts for check / checkmate
 
-                moveAsAlgebra = moveAsAlgebra.TrimEnd('#', '+');
-                Position to = board[moveAsAlgebra[^2..]].Position;
-                char pieceSymbol = char.ToUpper(moveAsAlgebra[0]);
-                if (to is not null)
+                // check that the algebra now ends with a valid position (e4 etc)
+                Position to;
+                try
                 {
-                    var pieces = validMoves.Where(m => m.To.Equals(to)).Select(m => m.Piece);
-                    if (pieces.Count() == 1)
+                    to = moveAsAlgebra[^2..];
+                }
+                catch
+                {
+                    throw new ArgumentException("Algebra must end with a valid position.");
+                }
+
+                char pieceSymbol = char.ToUpper(moveAsAlgebra[0]);
+                var pieces = validMoves.Where(m => m.To.Equals(to)).Select(m => m.Piece); // all pieces that *can* move to the target square
+                if (pieces.Count() == 1)
+                {
+                    piece = pieces.First(); // got it in one
+                }
+                else
+                {
+                    // we need the notation to identify the piece
+                    if (pieceSymbol == 'R' || pieceSymbol == 'N' || pieceSymbol == 'B' || pieceSymbol == 'Q' || pieceSymbol == 'K')
                     {
-                        piece = pieces.First();
-                    }
-                    else
-                    {
-                        if (pieceSymbol == 'R' || pieceSymbol == 'N' || pieceSymbol == 'B' || pieceSymbol == 'Q' || pieceSymbol == 'K')
+                        pieces = pieces.Where(p => p.Symbol == pieceSymbol);
+                        if (pieces.Count() == 1)
                         {
-                            pieces = pieces.Where(p => p.Symbol == pieceSymbol);
+                            piece = pieces.First(); // no further qualification needed
+                        }
+                        else
+                        {
+                            // have to be supplied the file of the piece moving
+                            pieces = pieces.Where(p => p.Square?.File == moveAsAlgebra[1]);
                             if (pieces.Count() == 1)
                             {
                                 piece = pieces.First();
                             }
                             else
                             {
-                                pieces = pieces.Where(p => p.Square?.File == moveAsAlgebra[1]);
-                                if (pieces.Count() == 1)
-                                {
-                                    piece = pieces.First();
-                                }
-                                else
-                                {
-                                    piece = pieces.SingleOrDefault(p => p.Square?.Rank == moveAsAlgebra[2] - '0');
-                                }
+                                // have to be supplied the rank of the piece moving
+                                piece = pieces.SingleOrDefault(p => p.Square?.Rank == moveAsAlgebra[2] - '0');
                             }
                         }
-                        else
-                        {
-                            piece = pieces.FirstOrDefault(p => p is Pawn);
-                        }
                     }
-
-                    if (piece is null)
+                    else
                     {
-                        throw new InvalidOperationException("Algebra cannot be parsed.");
+                        // it's got to be a pawn, so it's the first pawn that can move to this square (there should be only one of this colour)
+                        piece = pieces.FirstOrDefault(p => p is Pawn);
                     }
-
-                    move = new(piece, piece.Square.Position, to, takesPiece ? board[to].Piece : null);
                 }
+
+                if (piece is null)
+                {
+                    // oh well, we tried
+                    throw new InvalidOperationException("Algebra cannot be parsed.");
+                }
+
+                move = new(piece, piece.Square.Position, to, takesPiece ? board[to].Piece : null);
             }
             else
             {
