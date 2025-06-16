4fe8b0ab7471de5c2a5207eaaf40e5160adeb97f
Mark Lowe <mark.a.lowe@royalmail.com>
2025-06-16 13:02:16 +0100

    Add pgn converter

---
+++ Chess Challenge/RMGChess.Core/Converters/PGNConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMGChess.Core.Converters
+{
+    public static class PGNConverter
+    {
+        public static Dictionary<string, string[]> ConvertGame(string input)
+        {
+            var pgnLines = input.Split("\n");
+            var gameStart = Array.FindIndex(pgnLines, x => x == "");
+            var gameMovesString = pgnLines[gameStart + 1];
+            var gameMoves = gameMovesString.Split(" ").Where(x => !x.Contains(".")).SkipLast(1);
+
+
+            return new Dictionary<string, string[]> { { "Test Game", gameMoves.ToArray() } };
+        }
+    }
+}
