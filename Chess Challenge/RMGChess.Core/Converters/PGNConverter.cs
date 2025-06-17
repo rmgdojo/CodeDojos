0ad8bf17ba40f05fdaa55f9eeab0ef6e754fa499
Mark Lowe <mark.a.lowe@royalmail.com>
2025-06-17 09:07:17 +0100

    Parse entire PGN file and only play the first game for now as we need to fix the ambiguity

--- Chess Challenge/RMGChess.Core/Converters/PGNConverter.cs
+++ Chess Challenge/RMGChess.Core/Converters/PGNConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,30 @@ namespace RMGChess.Core.Converters
 {
     public static class PGNConverter
     {
+        private const string EventNameIdentifier = "[Event \"";
+        private const string DateIdentifier = "[Date \"";
+        private const string UtcTimeIdentifier = "[UTCTime \"";
+        private const string HeaderEnd = "\"]";
+
         public static Dictionary<string, string[]> ConvertGame(string input)
         {
             var pgnLines = input.Split("\n");
+
+            var gameName = pgnLines.FirstOrDefault(x => x.Contains(EventNameIdentifier))?.Replace(EventNameIdentifier, string.Empty).Replace(HeaderEnd, string.Empty);
+            var gameDate = pgnLines.FirstOrDefault(x => x.Contains(DateIdentifier))?.Replace(DateIdentifier, string.Empty).Replace(HeaderEnd, string.Empty);
+            var gameTime = pgnLines.FirstOrDefault(x => x.Contains(UtcTimeIdentifier))?.Replace(UtcTimeIdentifier, string.Empty).Replace(HeaderEnd, string.Empty);
+
             var gameStart = Array.FindIndex(pgnLines, x => x == "");
             var gameMovesString = pgnLines[gameStart + 1];
             var gameMoves = gameMovesString.Split(" ").Where(x => !x.Contains(".")).SkipLast(1);
 
+            return new Dictionary<string, string[]> { { $"{gameName} {gameDate} {gameTime}", gameMoves.ToArray() } };
+        }
 
-            return new Dictionary<string, string[]> { { "Test Game", gameMoves.ToArray() } };
+        public static List<Dictionary<string, string[]>> ConvertGames(string result)
+        {
+            var games = result.Split(new[] { "\n\n\n" }, StringSplitOptions.None);
+            return games.Where(x => !string.IsNullOrWhiteSpace(x)).Select(ConvertGame).ToList();
         }
     }
 }
